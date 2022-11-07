cd4de522b3a69ab2743b904ccb4adfdaa2b8210d
David Avalos <davalos@itparral.edu.mx>
2022-11-06 22:10:49 -0600

    wip: pantalla multimeda, casi termianda

--- screens/Multimedia.tsx
+++ screens/Multimedia.tsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import { ImageBackground, View, StyleSheet } from 'react-native'
-import { Button, Text } from '@ui-kitten/components'
+import { Button, Text, Layout, List, ListItem, Avatar } from '@ui-kitten/components'
 
+interface datosLista {
+  titulo: string,
+  descripcion: string,
+  imagen: 'audios' | 'videos' | 'imagenes' | 'documentos'
+}
+const datos:datosLista[] = [
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'audios'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'documentos'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'documentos'},
+  {titulo: 'Item 1', descripcion: 'Descripcion 1', imagen: 'documentos'},
+
+
+]
 
 type Props = {}
 
 export default function Multimedia({}: Props) {
+
+  const logoIzquierda = (props: any) => ( 
+    <Avatar {...props} style={{margin: 8}} size='giant' source={require('../assets/Imagenes/bocina.png')} />
+  );
+
+  const renderItem = ({ item, index }: {item:datosLista, index: number }) => (
+    <ListItem 
+      title={`${item.titulo} #${index + 1}`} 
+      description={item.descripcion}
+      accessoryLeft={logoIzquierda}
+      />
+  );
+
+
   return (
-    <View style={{flex:1}}>
-        <Text>Pantalla de Multimedia aqui</Text>
-    </View>
+    <Layout style={{flex:1}}>
+        <List data={datos} renderItem={renderItem} />
+    </Layout>
   )
 }
