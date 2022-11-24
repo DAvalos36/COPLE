9b7e3a74430493aca6d5231ab0e495018a4f4215
David Avalos <davalos@itparral.edu.mx>
2022-11-24 09:55:22 -0600

    avance

--- screens/Consejos.tsx
+++ screens/Consejos.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import { ImageBackground, View, StyleSheet } from 'react-native'
-import { Button, Text } from '@ui-kitten/components'
+import { Button, Text, Layout, List, ListItem, Avatar } from '@ui-kitten/components'
 
+interface datosLista {
+  titulo: string,
+  descripcion: string,
+  imagen: string
+}
+const datos:datosLista[] = [
+  {titulo: 'Identifica', descripcion: 'Descripcion 1', imagen: '../assets/Imagenes/indentificaIcono.png'},
+  {titulo: 'Conoce', descripcion: 'Descripcion 1', imagen: '../assets/Imagenes/conoceIcono.png'},
+  {titulo: 'Previene', descripcion: 'Descripcion 1', imagen: '../assets/Imagenes/previeneIcono.png'},
+  {titulo: 'Concentrate', descripcion: 'Descripcion 1', imagen: '../assets/Imagenes/concentrateIcono.png'},
+  {titulo: 'Acepta', descripcion: 'Descripcion 1', imagen: '../assets/Imagenes/aceptaIcono.png'},
+
+
+]
 
 type Props = {}
 
 export default function Consejos({}: Props) {
+
+  const logoIzquierda = ({a, props}: any) => ( 
+    <Avatar {...props} style={{margin: 8}} size='giant' source={require('../assets/Imagenes/bocina.png')} />
+  );
+
+  const renderItem = ({ item, index }: {item:datosLista, index: number }) => (
+    <ListItem 
+      title={<Text category='h3'>{item.titulo}</Text>} 
+      description={item.descripcion}
+      accessoryLeft={logoIzquierda}
+      />
+  );
+
+
   return (
-    <View style={{flex:1}}>
-        <Text>Pantalla de Consejos aqui</Text>
-    </View>
+    <Layout style={{flex:1}}>
+        <List data={datos} renderItem={renderItem} />
+    </Layout>
   )
 }
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,14 +1,63 @@
 import React from 'react'
 import { ImageBackground, View, StyleSheet } from 'react-native'
-import { Button, Text } from '@ui-kitten/components'
+import { Button, Text, BottomNavigation, Input, BottomNavigationTab, Icon, withStyles } from '@ui-kitten/components'
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs'
 
 
-type Props = {}
+type Props = {
+  navigation: BottomTabBarButtonProps
+}
 
-export default function Login({}: Props) {
+
+const PersonIcon = () => (
+  <Icon name='person-outline'/>
+);
+
+const BellIcon = () => (
+  <Icon name='bell-outline'/>
+);
+
+const EmailIcon = () => (
+  <Icon name='email-outline'/>
+);
+
+
+export default function Login({navigation}: Props) {
+  
+  // const bottomState = useBottomNa
+  
   return (
-    <View style={{flex:1}}>
+    <View style={{flex:1, justifyContent:'space-between', padding: 8}}>
         <Text>Pantalla de Login aqui</Text>
+        <View style={estilos.formulario}>
+          <Text>Aqui van botones!</Text>
+          <Text category='h4'>¡Bienvendio a la app!</Text>
+          <Input placeholder='E-Mail'style={estilos.inputs}/>
+          <Input placeholder='Contraseña'style={estilos.inputs}/>
+          <Button>abc</Button>
+          <Text>¿Olvidaste la contraseña?</Text>
+        </View>
+        
+
     </View>
   )
 }
+
+const estilos = StyleSheet.create({
+  formulario:{
+    flexDirection:'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginHorizontal: 40,
+    padding: 20,
+    height: 500,
+    backgroundColor: 'white',
+    borderRadius: 50,
+  },
+  inputs: {
+    borderRightWidth: 0,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+    backgroundColor: 'white'
+  }
+})
