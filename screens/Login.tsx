d41c79a7f9a5cce5b94e03ceaf06f4d0ae3c8680
David Avalos <davalos@itparral.edu.mx>
2022-11-28 00:26:30 -0600

    wip: login y registro

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { ImageBackground, View, StyleSheet } from 'react-native'
-import { Button, Text, BottomNavigation, Input, BottomNavigationTab, Icon, withStyles } from '@ui-kitten/components'
+import React, { useState } from 'react'
+import { ImageBackground, View, StyleSheet, ImageBackgroundComponent } from 'react-native'
+import { Button, Text, BottomNavigation, Input, BottomNavigationTab, Icon, Spinner } from '@ui-kitten/components'
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs'
+
+import { supabase } from '../supabase'
 
 
 type Props = {
@@ -23,22 +25,49 @@ const EmailIcon = () => (
 
 
 export default function Login({navigation}: Props) {
+  const [cargando, setCargando] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const iniciarSesion = () => {
+    setCargando(true)
+    supabase.auth.signInWithPassword({email, password}).then((response) => {
+      if(response.error) {
+        alert(response.error.message)
+      }
+      else {
+        alert('Usuario logueado')
+        console.log(response)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+    setCargando(false)
   
-  // const bottomState = useBottomNa
+  }
+  
+  const LoadingIndicator = (props: any) => (
+    <View style={[props.style]}>
+      <Spinner size='small'/>
+    </View>
+  );
   
   return (
-    <View style={{flex:1, justifyContent:'space-between', padding: 8}}>
-        <Text>Pantalla de Login aqui</Text>
+    <View style={{flex:1}}>
+      <ImageBackground source={require('../assets/Imagenes/bg-login-registro.png')} style={{ flex: 1, justifyContent:'flex-end', padding: 8, backgroundColor: '#E2DFDF' }} >
         <View style={estilos.formulario}>
-          <Text>Aqui van botones!</Text>
+          <View style={{ flexDirection:'row' }}>
+            <Button>a</Button>
+            <Button>b</Button>
+          </View>
+          {/* <Text>Aqui van botones!</Text> */}
           <Text category='h4'>¡Bienvendio a la app!</Text>
-          <Input placeholder='E-Mail'style={estilos.inputs}/>
-          <Input placeholder='Contraseña'style={estilos.inputs}/>
-          <Button>abc</Button>
+          <Input placeholder='E-Mail'style={estilos.inputs} onChangeText={(t) => {setEmail(t)}} />
+          <Input placeholder='Contraseña'style={estilos.inputs} onChangeText={(t) => {setPassword(t)}} />
+          <Button onPress={iniciarSesion} disabled={cargando} accessoryLeft={ cargando ? LoadingIndicator : undefined } >Iniciar sesion</Button>
           <Text>¿Olvidaste la contraseña?</Text>
         </View>
-        
-
+      </ImageBackground>
     </View>
   )
 }
@@ -48,11 +77,12 @@ const estilos = StyleSheet.create({
     flexDirection:'column',
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    marginHorizontal: 40,
+    marginHorizontal: 30,
     padding: 20,
     height: 500,
     backgroundColor: 'white',
     borderRadius: 50,
+    marginBottom: 24
   },
   inputs: {
     borderRightWidth: 0,
--- screens/Registro.tsx
+++ screens/Registro.tsx
@@ -1,14 +1,81 @@
 import React from 'react'
 import { ImageBackground, View, StyleSheet } from 'react-native'
-import { Button, Text } from '@ui-kitten/components'
+import { Button, Text, BottomNavigation, Input, BottomNavigationTab, Icon, withStyles } from '@ui-kitten/components'
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs'
+
+import { supabase } from '../supabase'
 
 
-type Props = {}
+type Props = {
+  navigation: BottomTabBarButtonProps
+}
 
-export default function Registro({}: Props) {
+
+const PersonIcon = () => (
+  <Icon name='person-outline'/>
+);
+
+const BellIcon = () => (
+  <Icon name='bell-outline'/>
+);
+
+const EmailIcon = () => (
+  <Icon name='email-outline'/>
+);
+
+
+export default function Registro({navigation}: Props) {
+  
+  // const bottomState = useBottomNa
+
+  const [email, setEmail] = React.useState<string>('')
+  const [password, setPassword] = React.useState<string>('')
+  const [loading, setLoading] = React.useState(false)
+
+  const registro = async () => {
+    setLoading(true)
+    supabase.auth.signUp({email, password}).then(({data, error}) => {
+      setLoading(false)
+      if (error) {
+        alert(error.message)
+      } else {
+        alert('Usuario creado')
+      }
+    })
+  }
+  
   return (
-    <View style={{flex:1}}>
-        <Text>Pantalla de Registro aqui</Text>
+    <View style={{flex:1, justifyContent:'space-between', padding: 8}}>
+        <Text>Pantalla de Login aqui</Text>
+        <View style={estilos.formulario}>
+          <Text>Aqui van botones!</Text>
+          <Text category='h4'>¡Bienvendio a la app!</Text>
+          <Input placeholder='E-Mail'style={estilos.inputs} onChangeText={(t) => {setEmail( t )}}/>
+          <Input placeholder='Contraseña'style={estilos.inputs} onChangeText={(t) => {setPassword(t)}} />
+          <Button onPress={registro} >Registrarme</Button>
+          <Text>¿Olvidaste la contraseña?</Text>
+        </View>
+        
+
     </View>
   )
 }
+
+const estilos = StyleSheet.create({
+  formulario:{
+    flexDirection:'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginHorizontal: 40,
+    padding: 20,
+    height: 500,
+    backgroundColor: 'white',
+    borderRadius: 50,
+  },
+  inputs: {
+    borderRightWidth: 0,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+    backgroundColor: 'white'
+  }
+})
